f071dbba723d88082a4f1036be79e8160c5c3056
hk7765738-sketch <hk7765738@gmail.com>
2026-07-13 19:19:23 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 122334
+let accountEmail = "Hamza@gmail.com"
+var accountPassword ="12345"
+accountCity ="Lahore"
+
+// accountId = 2
+accountEmail = "ali@gamil.com"
+accountPassword = "54321"
+accountCity = "Karachi"
+
+console.log("accountId");
+console.table([accountId, accountEmail, accountPassword, accountCity])
