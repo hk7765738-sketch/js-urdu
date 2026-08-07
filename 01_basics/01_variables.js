5cb5a8aac63806286838a3180fd42deeafbc005c
hk7765738-sketch <hk7765738@gmail.com>
2026-08-07 20:37:35 +0000

    done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,12 +1,17 @@
-const accountId = 122334
-let accountEmail = "Hamza@gmail.com"
-var accountPassword ="12345"
-accountCity ="Lahore"
+const accountId = 112233
+let accountEmail = "hk7765738@gmail.com"
+var accountPassWord = "364829HK"
+accountCity = "Lahore"
+let accountstate;
 
-// accountId = 2
-accountEmail = "ali@gamil.com"
-accountPassword = "54321"
-accountCity = "Karachi"
+// accountId = 332211 (Note: This is not allowed to change const value in js.)
 
-console.log("accountId");
-console.table([accountId, accountEmail, accountPassword, accountCity])
+/* Prefer not use var
+    because of issue in block scope and functional scope*/
+
+accountEmail = "8375677kh@gmail.com"
+accountPassWord = "KH928463"
+accountCity = "Islambad"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassWord, accountCity, accountstate]);
--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,29 +1,24 @@
-"use strict"; // treat all JS code as newer version
+"use strick"; // treat all JS code as newer version.
 
-// alert( 3 + 3) // we are using nodejs, not browser
+// alert(3+4) Note: We are using Node js, not browser.
 
-console.log(3 
-    +
-     3) // code readability should be high
+// console.log(3
+//     + 4
+// ); code readability should be high.
 
-console.log("Hitesh")
-
-
-let name = "hitesh"
-let age = 18
-let isLoggedIn = false
+let name = "Hamza"
+let age = 25
+let isLoggedIn = true
 let state;
 
 // number => 2 to power 53
-// bigint
-// string => ""
+// bigint => a big number/value
+// string => "" (value between this"")
 // boolean => true/false
 // null => standalone value
-// undefined => 
+// undefined => The value is not defined yet.
 // symbol => unique
-
-
 // object
 
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+console.log(typeof undefined); // undefined typeof is undefined
+console.log(typeof null); // null typeof is object
